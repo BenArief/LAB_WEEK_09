38fb85c337da84cfe4ad67433e701d83bcd02c12
BenArief <benarief29@gmail.com>
2025-11-11 20:18:17 +0700

    Commit No. 2 - State and Event Handler

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -17,8 +17,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.tooling.CompositionData
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,51 +42,83 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val list = listOf("Tanu", "Tina", "Tono")
-                    Home(list)
+                    Home()
                 }
             }
         }
     }
 }
+    data class Student(
+        var name: String
+    )
     @Composable
-    fun Home(
-        items: List<String>,
-        ) {
-            LazyColumn() {
-                item {
-                    Column(
-                        modifier = Modifier.padding(16.dp).fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(text = stringResource(id = R.string.enter_item))
-                        TextField(
-                            value = "",
-                            keyboardOptions = KeyboardOptions(
-                                keyboardType = KeyboardType.Number
-                            ),
-                            onValueChange = {}
-                        )
-                        Button(onClick = { }) {
-                            Text(text = stringResource(
-                                id = R.string.button_click)
-                            )
-                        }
-                    }
+    fun Home() {
+        val listData = remember {
+            mutableStateListOf(
+                Student("Tanu"),
+                Student("Tina"),
+                Student("Tono")
+            )
+        }
+        var inputField = remember { mutableStateOf(Student("")) }
+
+        HomeContent(
+            listData,
+            inputField.value,
+            {input -> inputField.value = inputField.value.copy(input)},
+            {
+                if (inputField.value.name.isNotBlank()){
+                    listData.add(inputField.value)
+                    inputField.value = Student("")
                 }
-                items(items) { item ->
-                    Column (
-                        modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(text = item)
+            }
+        )
+        }
+
+    @Composable
+    fun HomeContent(
+        listData: SnapshotStateList<Student>,
+        inputField: Student,
+        onInputValueChange: (String) -> Unit,
+        onButtonClick: () -> Unit
+    ) {
+        LazyColumn {
+            item {
+                Column(
+                    modifier = Modifier.padding(16.dp).fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = stringResource(id = R.string.enter_item))
+                    TextField(
+                        value = inputField.name,
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text
+                        ),
+                        onValueChange = {
+                            onInputValueChange(it)
+                        }
+                    )
+                    Button(onClick = {
+                        onButtonClick()
+                    }) {
+                        Text(text = stringResource(id = R.string.button_click))
                     }
                 }
             }
+            items(listData) {item ->
+                Column(
+                    modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = item.name)
+                }
+            }
+
         }
+    }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome(){
-    Home(listOf("Tanu", "Tina", "Tono"))
+    Home()
 }
